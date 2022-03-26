227f98b09f4577f196eeb239b3088374a3989f28
Bogdan Samonov <bogdansamonov@gmail.com>
2022-03-26 11:24:22 +0300

    feat: add logic to search

--- react/src/components/search/search.tsx
+++ react/src/components/search/search.tsx
@@ -1,13 +1,44 @@
-import { Component } from 'react';
+import { ChangeEvent, Component } from 'react';
 
 import './search.css';
 
 export default class Search extends Component {
+  state = {
+    label: '',
+  };
+
+  search = (e: ChangeEvent) => {
+    this.setState({
+      label: (e.target as HTMLInputElement).value,
+    });
+  };
+
+  componentDidMount() {
+    const label = localStorage.getItem('label');
+
+    if (label) {
+      const searchText = document.getElementById('search') as HTMLInputElement;
+      searchText.value = label;
+    }
+  }
+
+  componentWillUnmount() {
+    const { label } = this.state;
+
+    localStorage.setItem('label', label);
+  }
+
   render() {
     return (
       <form action="search">
         <div className="search-wrapper">
-          <input className="search-item" type="search" name="search" id="search" />
+          <input
+            className="search-item"
+            type="search"
+            name="search"
+            id="search"
+            onChange={this.search}
+          />
           <button className="search-btn">Search</button>
         </div>
       </form>
